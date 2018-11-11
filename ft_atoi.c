519d0cb4c918c4aef1811598f0f9ace222d3e0af
arkansio <mgessa@student.42.fr>
2018-11-11 20:34:17 +0100

    norme ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,19 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 20:55:51 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/11 20:16:01 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/11 20:31:43 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     ft_escape_seq(char c)
-{ 
-	return ((c == '\f' || c == 127)
-		   	|| (c == '\t' || c == '\n'))
-	   	|| ((c == '\r' || c == '\v')
-			   	|| (c == '\f' || c == ' '));
+static int	ft_escape_seq(char c)
+{
+	return (((c == '\f' || c == 127)
+				|| (c == '\t' || c == '\n'))
+			|| ((c == '\r' || c == '\v')
+				|| (c == '\f' || c == ' ')));
 }
 
-int		ft_atoi(const char *str)
+int			ft_atoi(const char *str)
 {
 	int		symbol;
 	int		value;

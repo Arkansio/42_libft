b12bef627be123c576d148a84f250320b0269a3d
arkansio <mgessa@student.42.fr>
2018-11-11 18:31:21 +0100

    correct functions

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 18:57:11 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/07 20:23:06 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/11 18:31:11 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,21 @@
 
 char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
-	size_t i;
-	size_t pos;
-	size_t pos_sub;
+	int     	sz;
+	size_t		i;
 
 	i = 0;
-	while (str[i] != '\0' && i < len)
+	if (ft_strlen(to_find) == 0)
+		return ((char *)str);
+	sz = ft_strlen(to_find);
+	if (sz == 1)
+		sz++;
+	sz--;
+	while (*str != '\0' && i++ < len)
 	{
-		if (str[i] == to_find[0])
-		{
-			pos = 0;
-			pos_sub = 0;
-			while (str[i + pos] == to_find[pos_sub])
-			{
-				pos++;
-				pos_sub++;
-				if (pos + 1 > ft_strlen(to_find))
-					return ((char*)&str[i]);
-			}
-		}
-		i++;
+		if (ft_strncmp(str, to_find, sz) == 0)
+			return ((char *)str);
+		str++;
 	}
-	return (NULL);	
+	return (NULL);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 18:34:48 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/10 18:20:04 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/11 18:24:47 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@ char    *ft_strstr(const char *str, const char *to_find)
 {
 	int		sz;
 
+	if (ft_strlen(to_find) == 0)
+		return ((char *)str);
 	sz = ft_strlen(to_find);
+	if (sz == 1)
+		sz++;
+	sz--;
 	while (*str != '\0')
 	{
 		if (ft_strncmp(str, to_find, sz) == 0)

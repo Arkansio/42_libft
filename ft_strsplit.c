33020b1568826e49742321fff79d8d721a3238ab
arkansio <mgessa@student.42.fr>
2018-11-12 18:05:23 +0100

    correct ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,20 +6,22 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 20:44:26 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/11 21:02:52 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/12 18:03:30 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+# include "libft.h"
+# include "stdlib.h"
+# include "stdio.h"
 
 static int		get_size(char const *s, char c)
 {
 	int		i;
 
 	i = 0;
-	while (s[i] != '\0' || s[i] != c)
+	while (s[i] != '\0' && s[i] != c)
 		i++;
-	return (s);
+	return (i);
 }
 
 static char		*get_next_word(char const *s, char c)
@@ -28,26 +30,63 @@ static char		*get_next_word(char const *s, char c)
 	char	*str;
 
 	size_word = get_size(s, c);
+	str = NULL;
 	if (!(str = ft_strnew(size_word)))
 		return (NULL);
 	str = ft_strncpy(str, s, size_word);
 	return (str);
 }
 
-char			**ft_strsplit(char const *s, char c)
+int				ft_count_words(char const *s, char c)
 {
 	int		i;
 	int		words;
 
+	words = 0;
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] = c)
+		if (s[i] != c)
 		{
-			if (get_next_word(&str[i], c))
-				words++;
+			i += get_size(&s[i], c);
+			words++;
 		}
+		else
+			i++;
 	}
-	printf("%d\n", words);
-	return (NULL);
+	if (s[i - 1] == c)
+		words++;
+	return (words);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		words;
+	char	**tab;
+	int		i_tab;
+
+	i_tab = 0;
+	words = ft_count_words(s, c);
+	printf("words %d\n",  words);
+	if (!(tab = malloc(sizeof(char*) * (words + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			tab[i_tab] = get_next_word(&s[i], c);
+			i_tab++;
+			i += get_size(&s[i], c);
+		}
+		else
+			i++;
+	}
+	if (s[i - 1] == c)
+		tab[i_tab++] = NULL;
+	if (!(tab[i_tab] = malloc(sizeof(char))))
+		return (NULL);
+	tab[i_tab][0] = '\0';
+	return (tab);
 }

8b26310f1d0920da34d802d7ce999d7c75352020
arkansio <mgessa@student.42.fr>
2018-11-14 17:22:28 +0100

    correct ft_memchr cast int to unsigned char

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 15:52:18 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/14 16:44:49 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/14 17:16:21 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t						i;
 	const unsigned char			*str;
+	unsigned char				ch;
 
+	ch = (unsigned char)c;
 	str = (const unsigned char*)s;
 	i = 0;
 	while (i < n)
 	{
-		if (str[i] == c)
+		if (str[i] == ch)
 			return ((void*)&str[i]);
 		i++;
 	}

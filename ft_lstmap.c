2b2f15bd7b4810725955f51bdf59b303b0d31014
arkansio <mgessa@student.42.fr>
2018-11-14 20:02:28 +0100

    free all the list when malloc failed & norme

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 19:27:32 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/14 19:54:38 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/14 20:01:00 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,27 @@
 static t_list	*ft_recursive(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new;
+	t_list	*tmp;
 
 	if (!(new = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
 	new = f(lst);
 	if (lst->next)
-		new->next = ft_recursive(lst->next, f);
+	{
+		tmp = ft_recursive(lst->next, f);
+		if (tmp == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->next = tmp;
+	}
 	return (new);
 }
 
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	if(!lst || !f)
+	if (!lst || !f)
 		return (NULL);
 	return (ft_recursive(lst, f));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 14:44:59 by mgessa            #+#    #+#             */
-/*   Updated: 2018/11/14 19:46:50 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/11/14 20:01:35 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,6 @@ void			ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list 			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
